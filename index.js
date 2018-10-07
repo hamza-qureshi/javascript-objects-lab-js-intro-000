15e4a68a761f4ef02e014ec03a115771dd701490
Hamza Qureshi <hamza.qureshi97@gmail.com>
2018-10-07 08:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
+
+function destructivelyUpadateObjectWithKeyAndValue (object, key, value) {
+  object.key = value 
+  return object
+}
+
+function deleteFromObjectByKey (object, key, value){
+  new newObj = Object.assign({}, object)
+  delete  newObj.key
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey (object, key) {
+  delete object.key
+  return object
+}
